08877cf6b02147567f46c1b34196f931d506bc1b
Angela Duran <duranangela19@gmail.com>
2018-12-13 19:13:25 -0700

    Updates

---
+++ precache-manifest.bd1a6a6b02e6d5cd7cb4fbedca85280f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "4e040ffa97e1ecac4554",
+    "url": "/todo/static/js/main.4e040ffa.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/todo/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "4e040ffa97e1ecac4554",
+    "url": "/todo/static/css/main.926bfb7b.chunk.css"
+  },
+  {
+    "revision": "b4a9f136128d203ca7e13333eaca63dd",
+    "url": "/todo/index.html"
+  }
+];
